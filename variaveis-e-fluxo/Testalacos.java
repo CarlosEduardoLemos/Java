e7c2af771bc6eabbbabcc942aa82c8b47405523d
Carlos Eduardo De Souza Lemos <lemosecarlos@gmail.com>
2023-01-06 16:50:28 -0300

    Update

---
+++ variaveis-e-fluxo/Testalacos.java
@@ -0,0 +1,11 @@
+public class Testalacos {
+    public static void main(String[] args) {
+        for (int multiplicador = 1; multiplicador <= 10; multiplicador++) {
+            for (int contador = 0; multiplicador <= 10; contador++) {
+                System.out.println(multiplicador * contador);
+                System.out.println("");
+            }
+            System.out.println();
+        }
+    }
+}
