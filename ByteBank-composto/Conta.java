9a77bc306495317444ad0bd196c88cf2b7cd39d3
Carlos Eduardo De Souza Lemos <lemosecarlos@gmail.com>
2023-01-07 19:57:37 -0300

    Update

--- ByteBank-composto/Conta.java
+++ ByteBank-composto/Conta.java
@@ -1,5 +1,5 @@
 public class Conta {
-    double saldo;
+    private double saldo;
     int agencia;
     int numero;
     //String titular;
@@ -26,5 +26,9 @@ public class Conta {
         }else{
             return false;
         }
+    }
+
+    public double pegaSaldo(){
+        return this.saldo;
     }
 }
