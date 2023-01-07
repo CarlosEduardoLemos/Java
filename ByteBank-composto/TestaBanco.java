89988275fe2989e56b22602f8a20f3d977d7baef
Carlos Eduardo De Souza Lemos <lemosecarlos@gmail.com>
2023-01-07 19:01:37 -0300

    Update

---
+++ ByteBank-composto/TestaBanco.java
@@ -0,0 +1,16 @@
+public class TestaBanco { 
+    public static void main(String[] args) { 
+        Cliente carlos = new Cliente(); 
+        carlos.nome = "Carlos Eduardo";
+        carlos.cpf = "222.222.222-22";
+        carlos.profissao = "programador";
+
+        Conta contaDoCarlos = new Conta(); 
+        contaDoCarlos.deposita(100);
+
+        contaDoCarlos.titular = carlos; 
+        System.out.println(contaDoCarlos.titular.nome);
+        System.out.println(contaDoCarlos.titular);
+        System.out.println(carlos);
+    }
+}
