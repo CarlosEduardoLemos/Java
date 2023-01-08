b5fa71465ea04498b995cdf793a25188f947efe7
Carlos Eduardo De Souza Lemos <lemosecarlos@gmail.com>
2023-01-07 23:56:30 -0300

    Update

---
+++ bytebank-encapsulado/Conta.java
@@ -0,0 +1,42 @@
+public class Conta {
+    private double saldo;
+    private int agencia;
+    private int numero;
+    //String titular;
+    private Cliente titular; //= new Cliente();
+
+    public void deposita(double valor){
+        this.saldo += valor;
+    }
+
+    public boolean saca(double valor){
+        if(this.saldo >= valor){
+            this.saldo -= valor;
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public boolean transfere(double valor, Conta destino){
+        if(this.saldo >= valor){
+            this.saldo -= valor;
+            destino.deposita(valor);
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public double pegaSaldo(){
+        return this.saldo;
+    }
+
+    public int getNumero(){
+        return this.numero;
+    }
+
+    public void setNumero(int novoNumero){
+        this.numero = novoNumero;
+    }
+}
---
+++ bytebank-encapsulado/TestaGetESet.java
@@ -0,0 +1,6 @@
+public class TestaGetESet {
+    public static void main(String[] args) {
+        Conta conta = new Conta();
+        conta.setNumero(1337);
+    }
+}
