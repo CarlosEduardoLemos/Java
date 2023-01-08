ba1a3fe476ff21cbc99071894a334719107205e8
Carlos Eduardo De Souza Lemos <lemosecarlos@gmail.com>
2023-01-08 13:15:40 -0300

    Update

--- bytebank-encapsulado/Conta.java
+++ bytebank-encapsulado/Conta.java
@@ -39,4 +39,22 @@ public class Conta {
     public void setNumero(int novoNumero){
         this.numero = novoNumero;
     }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(int agencia) {
+        this.agencia = agencia;
+    }
+
+    public void setTitular(Cliente titular) {
+        this.titular = titular;
+    }
+
+    public Cliente getTitular() {
+        return titular;
+    }
+
+
 }
--- bytebank-encapsulado/TestaGetESet.java
+++ bytebank-encapsulado/TestaGetESet.java
@@ -2,5 +2,15 @@ public class TestaGetESet {
     public static void main(String[] args) {
         Conta conta = new Conta();
         conta.setNumero(1337);
+        System.out.println(conta.getNumero());
+
+        Cliente carlos = new Cliente();
+        carlos.setNome("Carlos Eduardo"); 
+
+        conta.setTitular(carlos);
+
+        System.out.println(conta.getTitular().getNome());
+
+        conta.getTitular().setProfissao("Programador");
     }
 }
