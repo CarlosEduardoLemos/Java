92c57afba17ebc3d139fa090cd19329cc4af965d
Carlos Eduardo De Souza Lemos <lemosecarlos@gmail.com>
2023-05-17 11:04:24 -0300

    Update Main.java

--- Formação-Java-OO/src/Main.java
+++ Formação-Java-OO/src/Main.java
@@ -1,5 +1,11 @@
 public class Main{
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Filme: Top gun: Maverick");
+
+        int anoDeLancamento = 2022;
+        System.out.println("Ano de lançamento " + anoDeLancamento);
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.1;
     }
 }
