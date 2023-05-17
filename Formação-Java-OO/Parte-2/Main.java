b34d719ad0be87b9a278b3e356a36474e4bc2de8
Carlos Eduardo De Souza Lemos <lemosecarlos@gmail.com>
2023-05-17 18:41:16 -0300

    Update

--- Formação-Java-OO/Parte-2/Main.java
+++ Formação-Java-OO/Parte-2/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        
+        Filme meuFilme = new Filme();
+        meuFilme.nome = "O poderoso chefão";
+        meuFilme.anoDeLancamento = 1970;
+        meuFilme.duracaoEmMinutos = 180;
+
+        System.out.println(meuFilme.nome);
+        System.out.println(meuFilme.anoDeLancamento);
     }
 }
