75c53f1996c89d62b97113794979d036ebe87cd5
Carlos Eduardo De Souza Lemos <lemosecarlos@gmail.com>
2023-05-20 21:46:14 -0300

    Update

--- Formação-Java-OO/Parte-2/Main.java
+++ Formação-Java-OO/Parte-2/Main.java
@@ -1,11 +1,23 @@
-public class Main {
+import br.com.alura.screenmatch.modelos.Filme;
+
+public class Principal {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
-        meuFilme.nome = "O poderoso chefão";
-        meuFilme.anoDeLancamento = 1970;
-        meuFilme.duracaoEmMinutos = 180;
+        meuFilme.setNome("O poderoso chefão");
+        meuFilme.setAnoDeLancamento(1970);
+        meuFilme.setDuracaoEmMinutos(180);
 
-        System.out.println(meuFilme.nome);
-        System.out.println(meuFilme.anoDeLancamento);
+
+        meuFilme.exibeFichaTecnica();
+        meuFilme.avalia(8);
+        meuFilme.avalia(5);
+        meuFilme.avalia(10);
+        System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacoes());
+        System.out.println(meuFilme.pegaMedia());
+        //meuFilme.somaDasAvaliacoes = 10;
+        //meuFilme.totalDeAvaliacoes = 1;
+        //System.out.println(meuFilme.pegaMedia());
+
     }
+
 }
