793c85f120d74c83c8e478d6e679bfc6a53a6530
Carlos Eduardo De Souza Lemos <lemosecarlos@gmail.com>
2023-05-24 12:48:29 -0300

    Update

--- Formação-Java-OO/Parte-2/Modelos/Filme.java
+++ Formação-Java-OO/Parte-2/Modelos/Filme.java
@@ -26,4 +26,5 @@ public class Filme extends Titulo implements Classificavel {
     public String toString() {
         return "Filme: " + this.getNome() + " (" + this.getAnoDeLancamento() + ")";
     }
+
 }
